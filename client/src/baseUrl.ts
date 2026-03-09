4ca78d8b98603450c3e84ce36ab68287fce0e9ca
Tamás Major <majortamas313@gmail.com>
2026-03-09 23:04:42 +0100

    test and deploy

--- client/src/baseUrl.ts
+++ client/src/baseUrl.ts
@@ -1,6 +1,6 @@
 const isProduction = import.meta.env.PROD;
 
-const prod = "https://jerneif-backend.fly.dev"
+const prod = "https://fsiotserver.fly.dev"
 const dev = "http://localhost:5005"
 
 export const finalUrl = isProduction ? prod : dev
